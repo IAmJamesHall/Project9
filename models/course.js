e4dea3ad0bcd1f4ed8b53ba252bf532c97a1297b
IAmJamesHall <thatwhichendureth@gmail.com>
2020-03-26 14:46:53 -0400

    Model names were switched

--- models/course.js
+++ models/course.js
@@ -1,14 +1,38 @@
 const Sequelize = require('sequelize');
 
 module.exports = (sequelize) => {
-  class Course extends Sequelize.Model {};
+  class Course extends Sequelize.Model { };
   Course.init({
-    // TODO: fill out nullables
-    firstName: Sequelize.STRING,
-    lastName: Sequelize.STRING,
-    emailAddress: Sequelize.STRING,
-    password: Sequelize.STRING
-  }, { sequelize })
+    userId: {
+      type: Sequelize.INTEGER,
+      validate: {
+        notEmpty: {
+          msg: '"userId" is required'
+        }
+      }
+    },
+    title: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: '"title" is required'
+        }
+      }
+    },
+    description: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: '"description" is required'
+        }
+      }
+    },
+    estimatedTime: Sequelize.STRING,
+    materialsNeeded: Sequelize.STRING
+
+  }, { sequelize });
+
+  Course.belongsTo(User);
 
   return Course;
 }
--- models/user.js
+++ models/user.js
@@ -3,11 +3,46 @@ const Sequelize = require('sequelize');
 module.exports = sequelize => {
   class User extends Sequelize.Model {};
   User.init({
-    // TODO: fill out nullables
-    userId: Sequelize.INTEGER,
-    title: Sequelize.STRING,
-    description: Sequelize.STRING,
-    estimatedTime: Sequelize.STRING,
-    materialsNeeded: Sequelize.STRING
-  })
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: '"firstName" is required'
+        }
+      }
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: '"lastName" is required'
+        }
+      }
+    },
+    emailAddress: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: '"emailAddress" is required'
+        }
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: '"password" is required'
+        }
+      }
+    }
+  }, { sequelize });
+
+  User.hasMany(Course);
+
+  return User;
 }
